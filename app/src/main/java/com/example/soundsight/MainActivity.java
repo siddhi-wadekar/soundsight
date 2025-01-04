de23b48b95916824da2baf71cf7592e2fd94e9b0
Siddhi Wadekar <wadekarsiddhi2006@gmail.com>
2025-01-04 23:07:34 +0530

    event handling now, commit finished.

---
+++ app/src/main/java/com/example/soundsight/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.soundsight;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        // Change this to target a specific view ID in your layout.
+        // If you don't have a root view with the ID 'main', replace it with a valid view ID from activity_main.xml.
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(android.R.id.content), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
